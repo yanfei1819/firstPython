9321aad90308ce2fd3c81bafa13da15156beac37
shiyanfei <shiyanfei@keking.cn>
2018-03-21 18:27:14 +0800

    注释的形式

--- bascgrammar/day01/printTest.py
+++ bascgrammar/day01/printTest.py
@@ -2,7 +2,17 @@ print(121)
 print("hello,python")
 
 # Python中无{}，用缩进取代
+'''
+这是多行注释
+'''
+
+"""
+这也是多行注释
+"""
+
 if 3 > 9:
     print("this is true")
 else:
     print("this is false")
+    print("d")
+
